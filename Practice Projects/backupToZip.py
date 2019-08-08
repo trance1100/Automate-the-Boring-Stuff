add8b78d11f500bea048967f5dfa15eb8e2e9cf7
Trance1100 <trance1100@gmail.com>
2019-08-07 23:20:30 -0400

    idk

--- Practice Projects/backupToZip.py
+++ Practice Projects/backupToZip.py
@@ -22,7 +22,18 @@ def backupToZip(folder):
     print('Creating %s...' % (zipFilename))
     backupZip = zipfile.ZipFile(zipFilename, 'w')
     
-    # TODO: Walk the entire folder tree and compress the files in each folder.
+    # Walk the entire folder tree and compress the files in each folder.
+    for foldername, subfolder, filenames in os.walk(folder):
+        print('Adding files in %s...' % (foldername))
+        # Add the current folder to the ZIP file.
+        backupZip.write(foldername)
+        # Add all the files in this folder to the ZIP file.
+        for filename in filenames:
+            newBase = os.path.basename(folder) + '_'
+            if filename.startswith(newBase) and filename.endswith('.zip'):
+                continue # Dont backup the backup ZIP files
+            backupZip.write(os.path.join(foldername, filename))
+    backupZip.close()
     print('Done!')
 
 backupToZip('C:\\delicious')
